88d3410de7551f554a7504b39fb9568585cf4b28
ganeshrevadi <100305642+ganeshrevadi@users.noreply.github.com>
2022-09-11 10:28:02 +0530

    Add files via upload

---
+++ MathforDSA/Nonrep.java
@@ -0,0 +1,14 @@
+public class Nonrep {
+    public static void main(String[] args) {
+        int[] arr= {1,1,2,2,3,3,4};
+        System.out.println(ans(arr));
+    }
+
+    public static int ans(int[] arr) {
+        int unique =0;
+        for(int n :arr){
+            unique ^= n;
+        }
+        return unique;
+    }
+}
