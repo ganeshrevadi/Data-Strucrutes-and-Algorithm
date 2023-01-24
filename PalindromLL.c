7adfdf506b52485296577a09f37903725e413fe6
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-01-24 21:01:57 +0530

    Create PalindromLL.c

---
+++ PalindromLL.c
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ https://leetcode.com/problems/palindrome-linked-list/description/
+ */
+
+bool isPalindrome(struct ListNode* head){
+    struct ListNode *current,*new,*prev,*front;
+   new = NULL;
+   if(head == NULL){
+     return false;
+ }
+   current = head;
+
+   while(current){
+   front = current->next;
+   current->next = new;
+   new = current;
+   current = front;
+}
+ 
+ 
+ while(head && new){
+    
+     if(head->val != new->val){
+        
+         return false;
+     }
+     
+     else{
+        head = head->next;
+         new = new->next;
+         }
+    
+}
+
+return true;
+} 
