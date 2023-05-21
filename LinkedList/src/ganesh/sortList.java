cbd98be69c73359d4af940aa820f482ba5dc770a
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-05-21 20:43:00 +0530

    sortList

--- LinkedList/src/ganesh/sortList.java
+++ LinkedList/src/ganesh/sortList.java
@@ -1,6 +1,14 @@
 package ganesh;
 
+
 public class sortList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null){
             return head;
