492d873214c3a1d9b254160deff41c82666a18c1
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-06-05 15:57:09 +0530

    Stack

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(35);
+        stack.push(36);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        //System.out.println(stack.pop());
     }
 }
