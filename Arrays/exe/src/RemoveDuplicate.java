26fed9668f6683b3f903d2d808955bb0639b83bb
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-06-26 18:46:20 +0530

    Remove Duplicate

--- Arrays/exe/src/RemoveDuplicate.java
+++ Arrays/exe/src/RemoveDuplicate.java
@@ -1,8 +1,13 @@
 public class RemoveDuplicate {
     public static void main(String[] args) {
-         int[] nums = new int[]{1,1,2};
-        for (int i = 0; i < nums.length; i++) {
-
+         int[] nums = new int[]{0,1,2,2};
+         int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[j] = nums[i];
+                j++;
+            }
         }
+        System.out.println(j);
     }
 }
