a35cc46738137c8f5b4c3f25d873ee941e751561
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-06-26 19:01:42 +0530

    Pascal Triangle

--- Arrays/exe/src/PascalTriangle.java
+++ Arrays/exe/src/PascalTriangle.java
@@ -6,13 +6,25 @@ import java.util.List;
 public class PascalTriangle {
     public static void main(String[] args) {
         int numRows = 5;
-        List<Integer> w = new ArrayList<>();
-        List<List<Integer>> a = new ArrayList();
-        for(int i=1;i<10; i++){
-            // your logic here make use of Arrays.asList()
-            a.add(Arrays.asList(i, i+1));
+        List<List<Integer>> triangle = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            if (i > 0) {
+                List<Integer> prevRow = triangle.get(i - 1);
+                for (int j = 0; j < prevRow.size() - 1; j++) {
+                    int sum = prevRow.get(j) + prevRow.get(j + 1);
+                    row.add(sum);
+                }
+            }
+
+            if (i > 0) {
+                row.add(1);
+            }
+
+            triangle.add(row);
         }
-        System.out.println(a);
 
     }
 }
