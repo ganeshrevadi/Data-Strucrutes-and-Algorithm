c71b763623c2c4457deccaa6e45273f844aad3c5
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-06-27 20:03:06 +0530

    Merge

--- Arrays/exe/src/MergerIntervals.java
+++ Arrays/exe/src/MergerIntervals.java
@@ -3,11 +3,11 @@ import java.util.Set;
 import java.util.Arrays;
 public class MergerIntervals {
     public static void main(String[] args) {
-        int[][] intervals = new int[][]{{1,4},{0,2},{3,5}};
+        int[][] intervals = new int[][]{{1,4},{0,1}};
         int flag = 0;
-        int[][] newArray2D = new int[intervals.length - 1][];
         int n = intervals.length - 1;
         while(n != 0) {
+            int[][] newArray2D = new int[intervals.length - 1][];
             for (int i = 1; i < intervals.length; i++) {
                 if (intervals[i - 1][1] >= intervals[i][0] && intervals[i - 1][0] < intervals[i][1]) {
                     flag = 1;
@@ -24,6 +24,7 @@ public class MergerIntervals {
                 }
             }
             n--;
+            intervals = newArray2D;
         }
         if(flag == 0){
             for (int i = 0; i < intervals.length; i++) {
@@ -34,9 +35,9 @@ public class MergerIntervals {
             }
         }
         else{
-            for (int i = 0; i < newArray2D.length; i++) {
-                for (int j = 0; j < newArray2D[i].length; j++) {
-                    System.out.print(newArray2D[i][j] + " ");
+            for (int i = 0; i < intervals.length; i++) {
+                for (int j = 0; j < intervals[i].length; j++) {
+                    System.out.print(intervals[i][j] + " ");
                 }
                 System.out.println();
             }
