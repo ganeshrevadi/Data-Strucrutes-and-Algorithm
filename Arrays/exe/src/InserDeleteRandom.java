45b7a96a8a421f8aa0022ef989f078a995f02580
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-06-29 21:53:11 +0530

    Random

---
+++ Arrays/exe/src/InserDeleteRandom.java
@@ -0,0 +1,26 @@
+public class InserDeleteRandom {
+
+}
+class RandomizedSet{
+    public RandomizedSet() {
+        switch (RandomizedSet)
+    }
+
+    public boolean insert(int val) {
+        RandomizedSet obj = new RandomizedSet();
+        boolean param_1 = obj.insert(val);
+        return param_1;
+    }
+
+    public boolean remove(int val) {
+        RandomizedSet obj = new RandomizedSet();
+        boolean param_2 = obj.remove(val);
+        return param_2;
+    }
+
+    public int getRandom() {
+        RandomizedSet obj = new RandomizedSet();
+        int param_3 = obj.getRandom();
+        return param_3;
+    }
+}
