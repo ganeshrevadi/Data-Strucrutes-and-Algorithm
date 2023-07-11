0c3838eb0fc1fd75b93b3fb1621c01cb32d4d030
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-11 18:06:59 +0530

    Threesome closest

--- Arrays/exe/src/ThreeSumClosest.java
+++ Arrays/exe/src/ThreeSumClosest.java
@@ -1,21 +1,32 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
 public class ThreeSumClosest {
     public static void main(String[] args) {
-        int[] nums = {-1,2,1,-4};
+        int[] nums = {-1, 2, 1, -4};
         int target = 1;
+        int result = nums[0] + nums[1] + nums[nums.length -1];
+        Arrays.sort(nums);
         int sum = 0;
         int diff = 0;
-        int prevdif  = 0;
-        for(int i = 2;i<nums.length;i++){
-            sum = nums[i] + nums[i - 1] + nums[i - 2];
-            diff =Math.abs(target - sum);
-            if(prevdif > diff){
-                prevdif = diff;
+
+        for(int i = 0;i<nums.length - 2;i++){
+            int ptr1 = i + 1;
+            int ptr2 = nums.length -1;
+
+            while(ptr2 > ptr1){
+                sum = nums[i] + nums[ptr1] + nums[ptr2];
+                if(sum > target){
+                    ptr2--;
+                }
+                else{
+                    ptr1++;
+                }
+                if(Math.abs(sum - target) < Math.abs(result - target)){
+                    result = sum;
+                }
+
             }
-
         }
-        sum = Math.abs(sum);
-        System.out.println(sum);
-
+        System.out.println("The Closest result is : " + result);
     }
 }
