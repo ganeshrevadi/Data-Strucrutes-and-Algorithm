6f42252144776396c72c03e9db0df6d7cdf9c1a9
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-12 10:13:29 +0530

    HashMap

--- HashMap/src/src/implementation.java
+++ HashMap/src/src/implementation.java
@@ -1,7 +1,93 @@
 import java.util.*;
 
 public class implementation {
-    public static void main(String[] args) {
+    static class HashMap<K,V>{
+        private class Node{
+            K key;
+            V value;
 
+            public Node(K key,V value) {
+                this.value = value;
+                this.key = key;
+            }
+        }
+
+        private int n;
+        private int N;
+        private LinkedList<Node> buckets[];
+
+        @SuppressWarnings("unchecked")
+        public HashMap(){
+            this.N = 4;
+            this.buckets = new LinkedList[4];
+            for (int i = 0; i < 4; i++) {
+                this.buckets[i] = new LinkedList<>();
+
+            }
+
+        }
+
+        private int hashFunction(K key){
+            int bi = key.hashCode();
+            return Math.abs(bi) % N;
+        }
+
+        private int searchInLL(K key , int bi){
+            LinkedList<Node> ll = buckets[bi];
+            for (int i = 0; i < ll.size(); i++) {
+                if(ll.get(i).key == key){
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void rehash(){
+            LinkedList<Node> oldBucket[] = buckets;
+            buckets = new LinkedList[N* 2];
+
+            for (int i = 0; i < N * 2; i++) {
+                buckets[i] = new LinkedList<>();
+
+            }
+
+            for (int i = 0; i < oldBucket.length; i++) {
+                LinkedList<Node> ll = oldBucket[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.get(j);
+                    put(node.key, node.value);
+                }
+            }
+        }
+        public void put(K key , V value){
+            int bi = hashFunction(key);
+            int di = searchInLL(key,bi);
+
+            if(di == -1){
+                buckets[bi].add(new Node(key,value));
+                n++;
+            }
+            else{
+               Node node =  buckets[bi].get(di);
+                node.value = value;
+            }
+
+            double lambda = (double) n/N;
+            if(lambda > 2.0){
+
+            }
+
+        }
+
+        public boolean containsKey(K key){
+            return false;
+        }
+
+        public static void main(String[] args) {
+            HashMap<String,Integer> map = new HashMap<>();
+            map.put("India",190);
+            map.put("China",200);
+
+        }
     }
 }
