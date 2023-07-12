97029533669b7721f9ba22eb64ff939ff4403afb
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-12 18:32:07 +0530

    HashSet

---
+++ HashMap/src/src/sethash.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class sethash {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        System.out.println(set);
+        System.out.println(set.size());
+
+        Iterator it = set.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
+
+    }
+}
