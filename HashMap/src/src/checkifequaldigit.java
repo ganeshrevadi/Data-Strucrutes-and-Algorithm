93c3d1d55cd93accc8bcd7566d551d04b3ab5e62
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-13 22:30:14 +0530

    ao

--- HashMap/src/src/checkifequaldigit.java
+++ HashMap/src/src/checkifequaldigit.java
@@ -3,14 +3,27 @@ import java.util.HashMap;
 public class checkifequaldigit {
     public static void main(String[] args) {
         String num = "1210";
-        int count = 0;
-        char[] ch = num.toCharArray();
-
-        for (int i = 0; i < ch.length; i++) {
-            char co = (char) i;
-            System.out.println(co);
-
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<num.length();i++){
+            int ch=num.charAt(i)-'0';
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
+        int count = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if(map.containsKey(i)){
+                count = map.get(i);
+            }
+            else{
+                count = 0;
+            }
+            int x  =num.charAt(i) - '0';
+            if(x != count){
+                System.out.println(false);
+            }
+        }
+
+        System.out.println(true);
+
 
 
 
