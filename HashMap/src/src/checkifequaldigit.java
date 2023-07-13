d6d0c621819997f828e0444eb2bb9548d9417103
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-13 22:17:58 +0530

    check

---
+++ HashMap/src/src/checkifequaldigit.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class checkifequaldigit {
+    public static void main(String[] args) {
+        String num = "1210";
+        int count = 0;
+        char[] ch = num.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            char co = (char) i;
+            System.out.println(co);
+
+        }
+
+
+
+    }
+}
