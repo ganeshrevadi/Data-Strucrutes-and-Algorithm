b64f69271f1ba8ec3ac7697cb0b566556a2670f6
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-14 08:23:32 +0530

    Done

--- HashMap/src/src/validAnagram.java
+++ HashMap/src/src/validAnagram.java
@@ -9,38 +9,29 @@ public class validAnagram {
         char[] ch1 = s.toCharArray();
         char[] ch2 = t.toCharArray();
 
-        HashMap<String,Integer> map = new HashMap<String, Integer>();
-        HashMap<String,Integer> map2 = new HashMap<String, Integer>();
+        HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+        HashMap<Character,Integer> map2 = new HashMap<Character, Integer>();
 
-        for (int i = 0; i < ch1.length; i++) {
-            if(map.containsKey(ch1[i])){
-                map.put(String.valueOf(ch1[i]),map.get(ch1[i]) + 1);
-
-            }
-            else {
-                map.put(String.valueOf(ch1[i]), 1);
-            }
-        }
-        for (int i = 0; i < ch2.length; i++) {
-            if(map2.containsKey(ch2[i])){
-                map2.put(String.valueOf(ch2[i]),map2.get(ch2[i]) + 1);
-
-            }
-            else {
-                map2.put(String.valueOf(ch2[i]), 1);
-            }
+        for (char c : ch1) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
 
-//        for(String key : map.keySet()) {
-//            for (String key2 : map2.keySet()) {
-//                if (key  == key2 && map.get(key) == map2.get(key2)) {
-//                    System.out.println("Anagram");
-//                } else {
-//                    System.out.println(" Not Anagram");
-//                }
+        for (char c : ch2) {
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        }
+
+//        System.out.println(map);
+//        for (int i = 0; i < ch2.length; i++) {
+//            if(map2.containsKey(ch2[i])){
+//                map2.put(String.valueOf(ch2[i]),map2.get(ch2[i]) + 1);
+//
+//            }
+//            else {
+//                map2.put(String.valueOf(ch2[i]), 1);
 //            }
 //        }
+//        System.out.println(map2);
 
         boolean ans = map.equals(map2);
         System.out.println(ans);
