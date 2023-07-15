400b708b6b8db1308337d6ee0f52b40da306d89d
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-15 14:47:24 +0530

    subString

---
+++ HashMap/src/src/subStingLongest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class subStingLongest {
+    public static void main(String[] args) {
+        HashMap<Character , Integer> map = new HashMap<Character,Integer>();
+        String s = "abcabcbb";
+        int left = 0,right = 0;
+        int n = s.length();
+        int len = 0;
+
+        while(right < n){
+            if(map.containsKey(s.charAt(right))){
+                left = Math.max(map.get(s.charAt(right)) + 1,left);
+            }
+
+            map.put(s.charAt(right),right);
+            len  = Math.max(len,right - left + 1);
+            right++;
+        }
+        System.out.println(len);
+
+    }
+}
