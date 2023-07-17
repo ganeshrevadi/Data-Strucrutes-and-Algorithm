569fc252df10ca028c76707cc00cb95ff3c15c12
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-17 08:07:23 +0530

    heapSort

--- Algorithms/src/heapSort.java
+++ Algorithms/src/heapSort.java
@@ -2,34 +2,53 @@ import java.util.Arrays;
 
 public class heapSort {
     public static void main(String[] args) {
-        int[] arr = {10,30,50,20,35,15};
-        arr = heapify(arr,arr.length,0);
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        int n = arr.length;
+        heapSort ob = new heapSort();
+        ob.Sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static int[] heapify(int[] a,int n ,int i){
-        int largest = i;
-        int l = 2 * i;
-        int r = 2 * i + 1;
+    static void Sort(int arr[]) {
+        int n = arr.length;
 
-        if(l <= n  && a[l]>a[largest]){
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+
+        }
+
+
+    }
+    private static void heapify( int[] a, int n, int i){
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && a[l] > a[largest]) {
             largest = l;
         }
-        if(r<= n && a[r] > a[largest]){
+        if (r < n && a[r] > a[largest]) {
             largest = r;
         }
 
-        if(largest != i){
-            swap(a[largest],a[i]);
-            heapify(a,n,largest);
+        if (largest != i) {
+            int swap = a[i];
+            a[i] = a[largest];
+            a[largest] = swap;
+            heapify(a, n, largest);
         }
-        return a;
     }
 
-    static void swap(int b , int s){
-        int temp = b;
-        b = s;
-        s = temp;
 
-    }
 }
