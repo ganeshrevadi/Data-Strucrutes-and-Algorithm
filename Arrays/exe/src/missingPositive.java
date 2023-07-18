5ea566a77c84f962f59a1851a0b186b2440fd383
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-18 08:13:21 +0530

    missingPov

--- Arrays/exe/src/missingPositive.java
+++ Arrays/exe/src/missingPositive.java
@@ -1,33 +1,21 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class missingPositive {
     public static void main(String[] args) {
-        int[] rest = {3,4,-1,1};
-        Arrays.sort(rest);
-        int[] nums = new int[3];
-        int j = 0;
-        for (int i = 0; i < rest.length; i++) {
-            if(rest[i] > 0){
-                nums[j++] = rest[i];
-            }
-        }
+        int[] nums = {3,4,-1,1};
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        if(nums[0] > 0 && nums[0] != 1){
-            System.out.println(1);
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) minHeap.add(nums[i]);
         }
-        if(nums[0] == 0 && nums[nums.length - 1] == nums.length - 1){
-            System.out.println(nums.length);
-        }
-        if(nums[0] == 1 && nums[nums.length - 1] == nums.length){
-            System.out.println(nums.length + 1);
-        }
-        int res = 0;
-        for (int i = 1; i < nums.length ; i++) {
-            if (nums[i] != nums[i - 1]++ && nums[i] > 0) {
-                res = nums[i- 1]++;
-            }
-        }
+        int minMissing = 1;
 
-        System.out.println(res);
+        while(! minHeap.isEmpty()) {
+            int element = minHeap.poll();
+            if(element == minMissing) minMissing ++;
+            else if(element > minMissing) break;
+        }
+        System.out.println(minMissing);
     }
 }
