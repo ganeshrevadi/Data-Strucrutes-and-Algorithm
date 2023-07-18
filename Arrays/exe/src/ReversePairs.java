2416487b483a7b5ef35338977a039656fd2227a4
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-18 07:44:44 +0530

    RevPair

--- Arrays/exe/src/ReversePairs.java
+++ Arrays/exe/src/ReversePairs.java
@@ -1,33 +1,82 @@
-public class ReversePairs {
-    public static int count = 0;
-    public static int left = 0;
-    public static int right = 0;
-    public static void main(String[] args) {
-        int[] nums = {1,3,2,3,1};
-
-//        for(int i = 0;i < nums.length;i++){
-//            for(int j = i + 1;j<nums.length;j++){
+//public class ReversePairs {
+//    public static int count = 0;
+//    public static void main(String[] args) {
+//        int[] nums = {1,3,2,3,1};
+//        System.out.println(revPairCount(nums,0,nums.length - 1));
+//    }
 //
-//                if(nums[i] > (long)2 * nums[j]){
-//                    count++;
-//                }
-//            }
+//    static int revPairCount(int[] arr, int start,int end){
+//        if(start == end){
+//            return arr[start];
 //        }
-        count = findlargest(nums,0, nums.length - 1);
-        System.out.println(count);
+//
+//        int mid = (start + end) /2;
+//        int left = revPairCount(arr,start,mid);
+//        int right = revPairCount(arr,mid + 1,end);
+//
+//        if(arr[left] > 2 * arr[right]){
+//            return count++;
+//        }
+//
+//        return count;
+//    }
+//
+//}
+public class ReversePairs {
+    public static void main(String[] args) {
+        int[] nums = {2,4,3,5,1};
+        System.out.println(reversePairs(nums));
     }
-    static public int findlargest(int[] arr,int l,int hi){
-        if(l == hi){
-            return l;
+
+    public static int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    public static int mergeSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return 0;
         }
 
-        int mid =  (l + hi) / 2;
-        if(arr[left] > 2 * arr[right]){
-            count++;
-        }
-        left = findlargest(arr, l, mid);
-        right = findlargest(arr, mid + 1, hi);
+        int mid = left + (right - left) / 2;
+        int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
 
+        int i = left;
+        int j = mid + 1;
+        while (i <= mid && j <= right) {
+            if ((long) nums[i] > 2 * (long) nums[j]) {
+                count += mid - i + 1;
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        merge(nums, left, mid, right);
         return count;
+    }
+
+    public static void merge(int[] nums, int left, int mid, int right) {
+        int[] merged = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                merged[k++] = nums[i++];
+            } else {
+                merged[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) {
+            merged[k++] = nums[i++];
+        }
+
+        while (j <= right) {
+            merged[k++] = nums[j++];
+        }
+
+        System.arraycopy(merged, 0, nums, left, right - left + 1);
     }
 }
