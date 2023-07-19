9c49cf0c9e4b89cc7818cc75d071fdae6fccc81b
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-19 23:11:11 +0530

    valid Parenthese

--- Stack/src/validParentheses.java
+++ Stack/src/validParentheses.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class validParentheses {
     public static void main(String[] args) {
-        String s = "(])";
+        String s = "()";
         char[] ch = s.toCharArray();
         Stack<Character> stack = new Stack<Character>();
 
@@ -10,16 +10,22 @@ public class validParentheses {
             if(ch[i] =='(' || ch[i] == '{' || ch[i] == '['){
                 stack.push(ch[i]);
             }
-            else if(ch[i] == ')' && stack.peek() == '(' ){
-                stack.pop();
-            }
-            else if(ch[i] == '}' && stack.peek() == '{' ){
-                stack.pop();
-            }
-            else if(ch[i] == ']'  && !stack.isEmpty()){
-                char x = stack.pop();
-                if(x != ch[i]){
-                    System.out.println(false);
+            else{
+                if(ch[i] == ')' ){
+                    if( stack.isEmpty() || stack.pop() != ')'){
+                        System.out.println(false);
+                    }
+                }
+                if(ch[i] == '}' ){
+                    if( stack.isEmpty() || stack.pop() != '}'){
+                        System.out.println(false);
+                    }
+
+                }
+                if(ch[i] == ']' ){
+                    if( stack.isEmpty() || stack.pop() != ']'){
+                        System.out.println(false);
+                    }
                 }
             }
         }
