9ca73158355ea4e9528fbe5e83ade0be90dd68b8
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-21 00:10:36 +0530

    climb

--- Stack/src/climbingStairs.java
+++ Stack/src/climbingStairs.java
@@ -1,14 +1,16 @@
 public class climbingStairs {
     public static void main(String[] args) {
-        int n  = 3;
+        int n  = 36;
         System.out.println(rec(n));
     }
 
     public static int rec(int n){
-        if(n == 0 || n == 1){
-            return 1;
+        int[] count = new int[n+1];
+        count[0] = 1; count[1] = 1;
+        for(int i = 2; i<= n; i++) {
+            count[i] = count[i-1] + count[i-2];
         }
-        return rec(n-1) + rec(n-2);
+        return count[n];
     }
 }
 
