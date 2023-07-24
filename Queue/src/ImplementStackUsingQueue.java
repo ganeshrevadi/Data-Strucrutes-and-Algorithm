409eca15248a25ea7b69ef3693b18326f6bb7191
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-24 22:43:20 +0530

    ImplementStackUsingQueue

--- Queue/src/ImplementStackUsingQueue.java
+++ Queue/src/ImplementStackUsingQueue.java
@@ -2,27 +2,39 @@ import java.util.*;
 import java.util.Queue;
 
 public class ImplementStackUsingQueue {
-    public static void main(String[] args) {
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+    public ImplementStackUsingQueue() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
 
-    public static void push(int x, Queue<Integer> q1, Queue<Integer> q2) {
-        q1.add(x);
-    }
-
-    public static int pop(int x, Queue<Integer> q1, Queue<Integer> q2) {
-        while (!q1.isEmpty()) {
-            q2.add(q1.remove());
+    public void push(int x) {
+        q2.add(x);
+        while(!q1.isEmpty()){
+            q2.add(q1.peek());
+            q1.remove();
         }
-
-        return q2.remove();
+        Queue<Integer> q = q1;
+        q1 = q2;
+        q2 = q;
     }
 
-    public static int peek(int x, Queue<Integer> q1, Queue<Integer> q2) {
-        while (!q1.isEmpty()) {
-            q2.add(q1.remove());
+    public int pop() {
+        if(q1.isEmpty()){
+            return -1;
         }
-        return q2.peek();
+        return q1.remove();
+    }
+
+    public int top() {
+        if(q1.isEmpty()){
+            return -1;
+        }
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
     }
 }
