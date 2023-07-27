e2b76a8156cdf3ef67a8290800f3bea6481f691c
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-27 19:43:26 +0530

    insertionSort

---
+++ Algorithms/src/insertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        int[] nums = {5,2,4,3,1};
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static void sort(int[] nums){
+        int n = nums.length;
+        for(int i =  0; i < nums.length;i++){
+            int j  = i;
+            while(j > 0 && nums[j - 1] > nums[j]){
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
+                j--;
+            }
+
+        }
+    }
+}
