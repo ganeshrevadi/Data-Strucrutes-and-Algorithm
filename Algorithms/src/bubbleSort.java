bc93423f22d24c17170a01718a57f83eb8d5af10
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-27 19:53:42 +0530

    recursiveBubbleSort

--- Algorithms/src/bubbleSort.java
+++ Algorithms/src/bubbleSort.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class bubbleSort {
     public static void main(String[] args) {
         int[] nums = {5,2,4,3,1};
-        sort(nums);
+//        sort(nums);
+        bubble_sort(nums, nums.length);
         System.out.println(Arrays.toString(nums));
     }
     public static void sort(int[] nums){
@@ -17,5 +18,20 @@ public class bubbleSort {
                 }
             }
         }
+    }
+    public static void  bubble_sort(int arr[], int n) {
+        // Base Case: range == 1.
+        if (n == 1) return;
+
+        for (int j = 0; j <= n - 2; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        //Range reduced after recursion:
+        bubble_sort(arr, n - 1);
     }
 }
