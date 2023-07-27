fa1f3778a020d6d004d44fc731b8bf99575ecda5
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-27 20:00:38 +0530

    recursiveInsertionSort

--- Algorithms/src/insertionSort.java
+++ Algorithms/src/insertionSort.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class insertionSort {
     public static void main(String[] args) {
         int[] nums = {5,2,4,3,1};
-        sort(nums);
+//        sort(nums);
+        insertion_sort(nums,0, nums.length);
         System.out.println(Arrays.toString(nums));
 
     }
@@ -19,5 +20,21 @@ public class insertionSort {
                 j--;
             }
         }
+    }
+
+    public static void insertion_sort(int arr[], int i, int n) {
+
+        // Base Case: i == n.
+        if (i == n) return;
+
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+
+        insertion_sort(arr, i + 1, n);
     }
 }
