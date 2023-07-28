71aeff791dc5816417d28c779f100b1e6cf43656
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-28 19:02:35 +0530

    Binary

--- Algorithms/Search/src/BinarySearch.java
+++ Algorithms/Search/src/BinarySearch.java
@@ -1,6 +1,23 @@
 public class BinarySearch {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,};
+        int ans = binary(arr, 5);
+        System.out.println(ans);
+    }
 
-        System.out.println("Hello world!");
+    public static int binary(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target == arr[mid]) {
+                return mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 }
