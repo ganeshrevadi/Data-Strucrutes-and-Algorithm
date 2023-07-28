78e6870bc8a15d93df79b1453c3c66f37030c815
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-28 19:37:37 +0530

    lowerBound

---
+++ Algorithms/Search/src/lowerBound.java
@@ -0,0 +1,25 @@
+public class lowerBound {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5};
+        int ans = LB(arr, 3);
+        System.out.println(ans);
+    }
+
+    static int LB(int[] arr,int target){
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target <= arr[mid]) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else {
+                ans = mid;
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
