86e462fdefa6fc63c0617efa6ccf0c8fdf2942f5
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-31 08:18:27 +0530

    peakElement

--- Algorithms/Search/src/Leet/peakElement.java
+++ Algorithms/Search/src/Leet/peakElement.java
@@ -5,7 +5,30 @@ public class peakElement {
 
     }
 
-    static int findPeak(int[] arr){
+    static int findPeak(int[] nums){
+        int n = nums.length;
+        if(n == 1) return 0;
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[nums.length - 1] > nums[nums.length - 2]){
+            return nums.length - 1;
+        }
+        int low = 1;
+        int high = nums.length - 2;
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]){
+                return mid;
+            }
+            if(nums[mid] < nums[mid + 1] && nums[mid] > nums[mid - 1]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
         return -1;
     }
 }
