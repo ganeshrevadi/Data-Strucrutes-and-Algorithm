0f2875b688dc811a4e118102a33097c116dcd753
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-07-31 07:19:19 +0530

    single

---
+++ Algorithms/Search/src/Leet/sinlgEleInSortedArr.java
@@ -0,0 +1,11 @@
+package Leet;
+
+public class sinlgEleInSortedArr {
+    public static void main(String[] args) {
+
+    }
+    static int singleNonDuplicate(int[] nums){
+
+        return -1;
+    }
+}
