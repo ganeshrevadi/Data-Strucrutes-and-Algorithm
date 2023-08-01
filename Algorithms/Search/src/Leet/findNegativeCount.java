ef95c3cfc1aa48fb44ac4ce5bcce8bac42fab117
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-01 21:39:44 +0530

    findNegativeCount

---
+++ Algorithms/Search/src/Leet/findNegativeCount.java
@@ -0,0 +1,21 @@
+package Leet;
+
+public class findNegativeCount {
+    public static void main(String[] args) {
+        int[][] grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        System.out.println(Naive(grid));
+    }
+
+    static int Naive(int[][] grid){
+        int count = 0;
+
+        for(int i = 0 ; i < grid.length ;i++){
+            for(int j = 0 ; j < grid[i].length;j++){
+                if(grid[i][j] < 0 ){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
