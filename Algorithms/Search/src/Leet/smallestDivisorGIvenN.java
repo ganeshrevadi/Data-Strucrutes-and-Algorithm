6d9b09e53a4141fbc6bd7b9a26f9694fb672f0d2
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-02 08:15:57 +0530

    smallestDivisor

--- Algorithms/Search/src/Leet/smallestDivisorGIvenN.java
+++ Algorithms/Search/src/Leet/smallestDivisorGIvenN.java
@@ -2,27 +2,23 @@ package Leet;
 
 public class smallestDivisorGIvenN {
     public static void main(String[] args) {
-            int[] arr = {1,2,5,9};
-            int th = 6;
+            int[] arr = {44,22,33,11,1};
+            int th = 5;
         System.out.println(smallestDivisor(arr,th));
     }
 
     public static boolean possible(int[] nums, int threshold, int mid) {
         int res = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] < mid){
-                res++;
-            }
-            else{
-                res += Math.ceil(nums[i] / mid);
-            }
+                res += Math.ceil((double) nums[i] / (double) mid);
         }
         return res <= threshold;
     }
     public  static int smallestDivisor(int[] arr, int threshold) {
-        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
+        int n = arr.length;
+        if(n > threshold) return -1;
+        int low = 1, high = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            low = Math.min(low, arr[i]);
             high = Math.max(high, arr[i]);
         }
         while(low <= high){
