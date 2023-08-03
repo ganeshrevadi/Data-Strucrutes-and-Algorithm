c1db70dd47f7bfb79a25d2aee07a614711c16716
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-03 23:45:39 +0530

    Kth missing

--- Algorithms/Search/src/Leet/KthMissing.java
+++ Algorithms/Search/src/Leet/KthMissing.java
@@ -5,6 +5,7 @@ public class KthMissing {
         int[] arr = {2,3,4,7,11};
         int k = 5;
         System.out.println(BruteF(arr,k));
+        System.out.println(missingK(arr,arr.length,k));
     }
 
     static int BruteF(int[] arr,int k){
@@ -17,5 +18,19 @@ public class KthMissing {
             }
         }
         return k;
+    }
+
+    public static int missingK(int[] vec, int n, int k) {
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int missing = vec[mid] - (mid + 1);
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return k + high + 1;
     }
 }
