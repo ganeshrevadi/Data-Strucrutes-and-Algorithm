d06b9d51788a1eef8a2a47659c829c3b5be8f5c8
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-04 23:10:25 +0530

    Search2D Matrix

---
+++ Algorithms/Search/src/Leet/search2D.java
@@ -0,0 +1,30 @@
+package Leet;
+
+public class search2D {
+    public static void main(String[] args) {
+       int[][] arr =  {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+       int target = 15;
+        System.out.println(searchMatrix(arr,target));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0)  return false;
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int low = 0;
+        int high = n * m - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(matrix[mid/m][mid%m] == target){
+                return true;
+            } else if (matrix[mid/m][mid%m] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
