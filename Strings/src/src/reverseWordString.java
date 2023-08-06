95b1ff893af668488239f94205a328e524a4592a
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-06 19:45:59 +0530

    reverseWordString

--- Strings/src/src/reverseWordString.java
+++ Strings/src/src/reverseWordString.java
@@ -3,7 +3,6 @@ import java.util.Collections;
 public class reverseWordString {
     public static void main(String[] args) {
         String str = "a good   example";
-
         System.out.println(str.strip());
         int start = 0;
         int end = str.length();
