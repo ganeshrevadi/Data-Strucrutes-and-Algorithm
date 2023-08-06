f5c71bcd482a7ca02706d398b977b1ff6a430964
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-06 18:43:10 +0530

    remove Parentheses

--- Strings/src/src/removeParentheses.java
+++ Strings/src/src/removeParentheses.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class removeParentheses {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println(removeOuterParentheses("(())()"));
+    }
+
+    public static String removeOuterParentheses(String s) {
+        Stack<Character> bracket = new Stack<>();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(bracket.size()>0){
+                    sb.append(s.charAt(i));
+                }
+                bracket.push(s.charAt(i));
+            }else{
+                bracket.pop();
+                if(bracket.size()>0){
+                    sb.append(s.charAt(i));
+                }
+            }
+        }
+        return sb.toString();
     }
 }
