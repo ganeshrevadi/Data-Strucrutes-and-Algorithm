e04c3daf05919b82b029d0f701dc220adcdba561
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-06 19:30:35 +0530

    reverese

---
+++ Strings/src/src/reverseWordString.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+
+public class reverseWordString {
+    public static void main(String[] args) {
+        String str = "a good   example";
+
+        System.out.println(str.strip());
+        int start = 0;
+        int end = str.length();
+        int i = 0;
+
+    }
+}
