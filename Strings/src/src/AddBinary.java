02f47781a00a5c609e7c9c8025a4a38ab8f3b077
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-07 23:15:38 +0530

    addBinary

--- Strings/src/src/AddBinary.java
+++ Strings/src/src/AddBinary.java
@@ -1,28 +1,26 @@
 public class AddBinary {
     public static void main(String[] args) {
-        System.out.println(addStrings("1010","1011"));
+
+        System.out.println(addStrings("1111","1111"));
     }
     public static String addStrings(String num1, String num2) {
         StringBuilder sb = new StringBuilder();
-
         int i = num1.length() - 1, j = num2.length() - 1;
         int carry = 0;
 
-        while (i >= 0 || j >= 0) {
-            int sumNum = (num1.charAt(i--) - '0') + (num2.charAt(j--) - '0');
-            sumNum += carry;
-            if(sumNum <= 1){
-                sb.append(sumNum);
-            }
-            else{
-                carry = 1;
-                sb.append("0");
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
 
+            if (i >= 0) {
+                sum += num1.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += num2.charAt(j--) - '0';
             }
 
+            sb.append(sum % 2);
+            carry = sum / 2;
         }
-
-        if (carry != 0) sb.append(carry);
         return sb.reverse().toString();
     }
 }
