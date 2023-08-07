2f18fadfe3a79bb92c848f22cbee83bbe4bbf75d
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-07 09:23:05 +0530

    sum of String

---
+++ Strings/src/src/sumOfString.java
@@ -0,0 +1,12 @@
+public class sumOfString {
+    public static void main(String[] args) {
+        String num1 = "1";
+        String num2 = "1";
+        System.out.println(addStrings(num1,num2));
+    }
+
+    public static String addStrings(String num1, String num2) {
+        String ans = num1 + num2;
+        return ans;
+    }
+}
