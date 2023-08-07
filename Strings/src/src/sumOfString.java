8b098b9e233b091a803adefc48a409a43be77d65
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-07 09:40:16 +0530

    sumofString

--- Strings/src/src/sumOfString.java
+++ Strings/src/src/sumOfString.java
@@ -1,12 +1,27 @@
 public class sumOfString {
     public static void main(String[] args) {
-        String num1 = "1";
-        String num2 = "1";
+        String num1 = "6913259244";
+        String num2 = "71103343";
         System.out.println(addStrings(num1,num2));
     }
 
     public static String addStrings(String num1, String num2) {
-        String ans = num1 + num2;
-        return ans;
+        StringBuilder sb = new StringBuilder();
+
+        int i = num1.length() - 1, j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+
+            if (i >= 0) sum += (num1.charAt(i--) - '0');
+            if (j >= 0) sum += (num2.charAt(j--) - '0');
+
+            sb.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
     }
 }
