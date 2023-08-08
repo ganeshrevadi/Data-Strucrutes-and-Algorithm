b1ae4fc84705c5a87999f585a21dab0834831fab
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-08 22:27:45 +0530

    reverse Word

--- Strings/src/src/LargestOddNumber.java
+++ Strings/src/src/LargestOddNumber.java
@@ -1,23 +1,25 @@
 public class LargestOddNumber {
     public static void main(String[] args) {
-        System.out.println(largestOddNumber("35427"));
+        System.out.println(largestOddNumber("10133890"));
     }
 
     public static String largestOddNumber(String num) {
-        if(Integer.valueOf(num) % 2 != 0){
+        if(Long.valueOf(num) % 2 != 0){
             return num;
         }
 
         String res = new String();
         int max = 0;
+        int flag = 0;
         for (int i = 0; i < num.length(); i++) {
             int od  = num.charAt(i) - '0';
-            if(od % 2 != 0 && od > max){
-                max = od;
+            if(od % 2 != 0){
+                flag = 1;
+                max = i;
             }
         }
-        if(max == 0) return "";
+        if(flag == 0) return "";
 
-        return String.valueOf(max);
+        return num.substring(0,max + 1);
     }
 }
