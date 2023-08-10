420efa14eb4c898898d03e4961561346be2cdb70
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-10 20:54:55 +0530

    Frequency Sort

--- Strings/src/src/FrequencySort.java
+++ Strings/src/src/FrequencySort.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class FrequencySort {
     public static void main(String[] args) {
@@ -20,10 +22,19 @@ public class FrequencySort {
 
         StringBuilder sb = new StringBuilder();
         int max = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if(max > 0){
-                sb.append(s.charAt(i));
+        while (!map.isEmpty()) {
+            char maxFreqChar = '\0';
+            int maxFreq = 0;
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > maxFreq) {
+                    maxFreq = entry.getValue();
+                    maxFreqChar = entry.getKey();
+                }
             }
+            for (int i = 0; i < maxFreq; i++) {
+                sb.append(maxFreqChar);
+            }
+            map.remove(maxFreqChar);
         }
         return sb.toString();
     }
