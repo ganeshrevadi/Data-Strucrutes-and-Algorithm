5a8788fe881b4b539a38b16cde37e88b1405dd43
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-15 07:23:03 +0530

    Microsoft

--- Arrays/exe/src/Microsoft1.java
+++ Arrays/exe/src/Microsoft1.java
@@ -2,6 +2,29 @@ public class Microsoft1 {
     public static void main(String[] args) {
     int[] A = {5,1,2,6,6,1,3,1,4,3,4,3,4,6,1,2,4,1,6,2};
         System.out.println(solution(A));
+        System.out.println(solutino(A));
+    }
+
+    public static int solutino(int[] A){
+        int count1 = 0;
+        int count2 = 0;
+        int len = A.length;
+        for (int i = 0; i < A.length - 1; i++) {
+            int ref = A[i];
+            for (int j = i + 1; j < A.length; j++) {
+                if (i % 2 != 0 && j % 2 == 0 && ref == A[j]) {
+                    count1++;
+                    ref = A[j + 1];
+                } else if (i % 2 == 0 && j % 2 != 0 && ref == A[j]) {
+                    count2++;
+                    ref = A[j - 1];
+                }
+            }
+        }
+        int min = Math.min(count1, count2); // Use Math.min to find the smaller count
+        len /= 2;
+        return len - min; // Remove the "- 1" here
+
     }
 
     public static int solution(int[] A) {
