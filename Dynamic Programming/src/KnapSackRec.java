0e5f9630b6a8b1b8385a7c6f791396df2f79423c
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-25 18:47:22 +0530

    Dynamic Programming

---
+++ Dynamic Programming/src/KnapSackRec.java
@@ -0,0 +1,22 @@
+public class KnapSackRec {
+    public static void main(String[] args) {
+
+        int val[] = { 60, 100, 120 };
+        int wt[] = { 10, 20, 30 };
+        int W = 50;
+        int n = val.length;
+        System.out.println(knapsack(wt,val,W,n));
+    }
+
+    public static int knapsack(int[] wt,int[] val,int W,int n){
+        if( n == 0 || W == 0){
+            return 0;
+        }
+
+        if(wt[n - 1] <= W){
+            return Math.max(val[n - 1] + knapsack(wt , val , W - wt[n - 1],n - 1),knapsack(wt,val,W,n - 1));
+        } else{
+            return knapsack(wt,val,W,n - 1);
+        }
+    }
+}
