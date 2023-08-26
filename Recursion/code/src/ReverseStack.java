72018d4e83687f9888a40c051f07059c94083186
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-26 18:29:54 +0530

    Reverse Stack

---
+++ Recursion/code/src/ReverseStack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class ReverseStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(0);
+        st.push(5);
+        st.push(2);
+        reverse(st);
+        System.out.println(st);
+        System.out.println(st.peek());
+    }
+
+
+    static void reverse(Stack<Integer> st){
+        if(st.isEmpty()){
+            return;
+        }
+        int temp = st.peek();
+        st.pop();
+        reverse(st);
+        insert(st,temp);
+    }
+    static void insert(Stack<Integer> st, int temp){
+        if(st.isEmpty()){
+            st.push(temp);
+            return;
+        }
+        int val = st.peek();
+        st.pop();
+        insert(st,temp);
+        st.push(val);
+        return;
+    }
+}
