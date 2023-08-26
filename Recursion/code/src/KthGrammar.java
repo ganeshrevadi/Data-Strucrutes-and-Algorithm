0e67a5f07bd85526a9e7c481d2f70b22b35ae48d
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-26 23:38:37 +0530

    KthGrammeer

--- Recursion/code/src/KthGrammar.java
+++ Recursion/code/src/KthGrammar.java
@@ -1,13 +1,13 @@
 public class KthGrammar {
     public static void main(String[] args) {
-        int n = 1;
+        int n = 2;
         int k = 2;
         System.out.println(kthGrammar(n,k));
     }
 
 
     public static int kthGrammar(int n, int k) {
-        if(n ==  1 && k == 1 ){
+        if(n ==  1 ||  k == 1 ){
             return 0;
         }
         int mid = (int)Math.pow(2,n - 1) / 2;
