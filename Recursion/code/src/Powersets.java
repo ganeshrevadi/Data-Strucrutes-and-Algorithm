c2e406854f04d8a26b309f21a4701f5e2f02afcc
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-27 18:18:35 +0530

    Powerset

--- Recursion/code/src/Powersets.java
+++ Recursion/code/src/Powersets.java
@@ -1,12 +1,17 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Powersets {
     public static void main(String[] args) {
-        solve("abababab","");
+        HashSet<String> map = new HashSet<>();
+        solve("aab","",map);
+        System.out.println(map );
     }
 
-    static void solve(String ip , String op){
+    static void solve(String ip , String op,HashSet<String> map){
         StringBuilder ip1 = new StringBuilder(ip);
         if(ip1.length() == 0){
-            System.out.println(op);
+            map.add(op);
             return;
         }
 
@@ -15,8 +20,8 @@ public class Powersets {
 
         op2.append(ip1.charAt(0));
         ip1.deleteCharAt(0);
-        solve(ip1.toString(), op1.toString());
-        solve(ip1.toString(),op2.toString());
+        solve(ip1.toString(), op1.toString(), map);
+        solve(ip1.toString(),op2.toString(), map);
         return;
     }
 }
