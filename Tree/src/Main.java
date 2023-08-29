701c62222cd0e6521b561dd0c35ddce5170bff0c
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-29 22:24:12 +0530

    Tree

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -1,5 +1,16 @@
+class Node{
+    int val;
+    Node left , right;
+    public void Node(int val){
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
+        Node tree = new Node();
+        tree.Node(5);
+        tree.left = null;
+        System.out.println(tree);
     }
 }
