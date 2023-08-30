68ae8016fd3198ba321b1f90590e5fb356b4108b
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-30 06:45:53 +0530

    Tree Shubahramb

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -1,15 +1,20 @@
-class Node{
+class Node {
     int val;
-    Node left , right;
-    public void Node(int val){
+    Node left, right;
+
+    public Node(int val) {
         this.val = val;
         this.left = this.right = null;
+    }
+    public Node(int val , Node left, Node right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
 public class Main {
     public static void main(String[] args) {
-        Node tree = new Node();
-        tree.Node(5);
+        Node tree = new Node(5);
         tree.left = null;
         System.out.println(tree);
     }
