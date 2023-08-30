b9c8a97172c24c2259233196c4c3e0a7c9588993
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-30 07:28:48 +0530

    Implementation of Trees

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -1,21 +1,49 @@
-class Node {
-    int val;
-    Node left, right;
+import java.util.Scanner;
 
-    public Node(int val) {
-        this.val = val;
-        this.left = this.right = null;
+class BinaryTree {
+    public BinaryTree() {
     }
-    public Node(int val , Node left, Node right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    private static class Node{
+        int val;
+        Node left , rigth;
+
+        public Node(int val) {
+            this.val = val;
+        }
     }
+    private Node root;
+    public void populate(Scanner scanner) {
+        System.out.println("Enter the root node: ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner,root);
+    }
+    private void populate(Scanner scanner, Node node){
+        System.out.println("Do you want to enter left of : " + node.val);
+        boolean left =  scanner.nextBoolean();
+        if(left){
+            System.out.println("Enter the value of the left of " + node.val);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner,node.left);
+        }
+
+        System.out.println("Do you want to enter right of : " + node.val);
+        boolean right =  scanner.nextBoolean();
+        if(right){
+            System.out.println("Enter the value of the right of " + node.val);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner,node.rigth);
+        }
+
+    }
+
+
+
 }
 public class Main {
     public static void main(String[] args) {
-        Node tree = new Node(5);
-        tree.left = null;
-        System.out.println(tree);
+
     }
 }
