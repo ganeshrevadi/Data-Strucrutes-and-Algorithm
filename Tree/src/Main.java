b88e46ef29c0dd911c618b6e80af1204140dfd48
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-31 07:41:01 +0530

    Binary Search Tree

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -85,7 +85,9 @@ public class Main {
 
         BST tree = new BST();
         int[] nums = {5,2,7,1,4,6,9,8,3,10};
+        int[] nums2 = {1,2,3,4,5,6,7,8,9,10};
         tree.populate(nums);
+        tree.populatedSorted(nums2);
         tree.display();
         tree.preetyDisplay();
     }
