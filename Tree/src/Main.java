1326fd9b80a04312a7dd8683b007729dc1d8c724
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-08-31 07:26:38 +0530

    Binary Search Tree

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -77,9 +77,15 @@ class BinaryTree {
 }
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(scanner);
+//        Scanner scanner = new Scanner(System.in);
+//        BinaryTree tree = new BinaryTree();
+//        tree.populate(scanner);
+//        tree.display();
+//        tree.preetyDisplay();
+
+        BST tree = new BST();
+        int[] nums = {5,2,7,1,4,6,9,8,3,10};
+        tree.populate(nums);
         tree.display();
         tree.preetyDisplay();
     }
