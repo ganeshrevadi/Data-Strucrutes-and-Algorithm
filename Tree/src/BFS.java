1d50575e00bb99f78ca3958627ba4ac32418ac32
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-09-01 07:44:36 +0530

    BFS

--- Tree/src/BFS.java
+++ Tree/src/BFS.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class BFS {
     public static void main(String[] args) {
 
-        int[] nums = {3,9,20,15,7};
+        int[] nums = {10, 20, 30, 40, 60};
         BST tree = new BST();
         tree.populate(nums);
         System.out.println(tree.levelOrder(tree.root));
