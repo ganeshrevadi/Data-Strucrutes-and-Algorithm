9fccef1be2c353511cbf8a4edcd378d7c92a449c
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-09-02 12:44:18 +0530

    Maximum Path Sum

---
+++ Tree/src/DiameterofBT.java
@@ -0,0 +1,28 @@
+
+public class DiameterofBT {
+    public static int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int max = maxDepth(root.right) + maxDepth(root.left);
+        return Math.max(max,Math.max(diameterOfBinaryTree(root.left), diameterOfBinaryTree(root.right)));
+    }
+
+
+    public static int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(4);
+        tree.insert(5);
+        tree.preetyDisplay();
+    }
+}
