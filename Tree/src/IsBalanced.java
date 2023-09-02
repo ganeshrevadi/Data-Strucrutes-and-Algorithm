9bb168bb9b3cd633cd7dc2e5803ba1fa059a0c73
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-09-02 06:06:42 +0530

    isBalanced

--- Tree/src/IsBalanced.java
+++ Tree/src/IsBalanced.java
@@ -12,17 +12,22 @@ class TreeNode {
  }
 
 public class IsBalanced {
-    public boolean isBalanced(TreeNode root) {
+    public static boolean isBalanced(TreeNode root) {
         if(root == null){
             return true;
         }
         return Math.abs(maxDepth(root.left) - maxDepth(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
     }
 
-    public int maxDepth(TreeNode root) {
+    public static int maxDepth(TreeNode root) {
         if(root == null){
             return 0;
         }
         return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(10);
+        System.out.println(isBalanced(tree));
     }
 }
