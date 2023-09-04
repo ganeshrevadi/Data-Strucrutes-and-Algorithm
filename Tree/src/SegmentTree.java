6e09f527f46e4f521325ef8d13678caf1ead1ede
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-09-04 07:56:29 +0530

    Segment Trees

--- Tree/src/SegmentTree.java
+++ Tree/src/SegmentTree.java
@@ -27,7 +27,7 @@ class SegmentTree {
         }
         // create new node with index you are at
         Node node = new Node(start,end);
-        int mid = start + (end - start ) / 2;
+        int mid = (start + end) / 2;
 
         node.left = this.constructTree(arr,start,mid);
         node.right = this.constructTree(arr,mid + 1 , end);
@@ -36,9 +36,77 @@ class SegmentTree {
         return node;
     }
 
+    public void display() {
+        display(this.root);
+    }
+    private void display(Node node) {
+        String str = "";
+
+        if(node.left != null) {
+            str = str + "Interval=[" + node.left.startInterval + "-" + node.left.endInterval + "] and data: " + node.left.val + " => ";
+        } else {
+            str = str + "No left child";
+        }
+
+        // for current node
+        str = str + "Interval=[" + node.startInterval + "-" + node.endInterval + "] and data: " + node.val + " <= ";
+        if(node.right != null) {
+            str = str + "Interval=[" + node.right.startInterval + "-" + node.right.endInterval + "] and data: " + node.right.val;
+        } else {
+            str = str + "No right child";
+        }
+
+        System.out.println(str + '\n');
+
+        // call recursion
+        if(node.left != null) {
+            display(node.left);
+        }
+
+        if(node.right != null) {
+            display(node.right);
+        }
+    }
+
+    // query
+    public int query(int qsi, int qei) {
+        return this.query(this.root, qsi, qei);
+    }
+    private int query(Node node, int qsi, int qei) {
+        if(node.startInterval >= qsi && node.endInterval <= qei) {
+            // node is completely lying inside query
+            return node.val;
+        } else if (node.startInterval > qei || node.endInterval < qsi) {
+            // completely outside
+            return 0;
+        } else {
+            return this.query(node.left, qsi, qei) + this.query(node.right, qsi, qei);
+        }
+    }
+
+    // update
+    public void update(int index, int value) {
+        this.root.val = update(this.root, index, value);
+    }
+    private int update(Node node, int index, int value) {
+        if (index >= node.startInterval&& index <= node.endInterval){
+            if(index == node.startInterval && index == node.endInterval) {
+                node.val = value;
+                return node.val;
+            } else {
+                int leftAns = update(node.left, index, value);
+                int rightAns = update(node.right, index, value);
+                node.val = leftAns + rightAns;
+                return node.val;
+            }
+        }
+        return node.val;
+    }
     public static void main(String[] args) {
-        int[] arr = {-3,8,6,7,-2,-8,4,9};
+        int[] arr = {3,8,6,7,-2,-8,4,9};
 
         SegmentTree tree = new SegmentTree(arr);
+    tree.display();
+        System.out.println(tree.query(1,6));
     }
 }
