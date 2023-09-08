3fc45aeef8d155ab5d283e746ae99287f821f090
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-09-08 11:48:53 +0530

    Sub Tree of Another Tree

--- Tree/src/SubTree.java
+++ Tree/src/SubTree.java
@@ -1,9 +1,8 @@
 
 public class SubTree {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null){
-            return false;
-        }
+        if(root==null && subRoot ==null|| root!=null && subRoot==null) return true;
+        if (root==null && subRoot!=null) return false;
         if(isIdentical(root,subRoot)){
             return true;
         }
@@ -12,11 +11,38 @@ public class SubTree {
 
     public boolean isIdentical(TreeNode root , TreeNode subTree){
         if(root == null  ||  subTree == null){
-            return root == null && subTree == null;
+            return true;
         }
+        if(root == null && subTree != null || root != null && subTree == null)  return false;
+
+        if(root.val != subTree.val) return false;
+
         return  root.val == subTree.val && isIdentical(root.left,subTree.left) && isIdentical(root.right,subTree.right);
     }
 
 
+}
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
+        if(root==null && subRoot ==null|| root!=null && subRoot==null) return true;
+        if (root==null && subRoot!=null) return false;
+
+        if(isSame(root,subRoot)) return true;
+
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+    }
+
+
+    private boolean isSame(TreeNode p, TreeNode q){
+
+        if(p==null && q==null) return true;
+        if(p==null && q!=null || p!=null && q==null) return false;
+
+        if(p.val!=q.val) return false;
+
+        return isSame(p.left,q.left) && isSame(p.right,q.right);
+    }
 }
 
