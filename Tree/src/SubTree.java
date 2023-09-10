14c6f434c644bf0d7fc372eea16361a6964ca1eb
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-09-10 10:07:52 +0530

    Sub Tree

--- Tree/src/SubTree.java
+++ Tree/src/SubTree.java
@@ -37,8 +37,8 @@ class Solution {
 
     private boolean isSame(TreeNode p, TreeNode q){
 
-        if(p==null && q==null) return true;
-        if(p==null && q!=null || p!=null && q==null) return false;
+        if(p == null && q == null) return true;
+        if(p == null && q != null || p != null && q == null) return false;
 
         if(p.val!=q.val) return false;
         System.out.println("Done");
