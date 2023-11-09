ff309e46eddea74f77748f9816cee7684a5284b0
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-11-09 10:16:32 +0530

    Interview Bit Array 01 & 02

--- Arrays/exe/src/Array_2D.java
+++ Arrays/exe/src/Array_2D.java
@@ -1,33 +1,32 @@
 import java.util.*;
 public class Array_2D {
     public static void main(String[] args) {
-//        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-//        A.add(0 ,new ArrayList<>());
-//        A.add(1,new ArrayList<>());
-//        A.add(2 , new ArrayList<>());
-//        A.get(0).add(1);
-//        A.get(0).add(2);
-//        A.get(0).add(3);
-//        A.get(0).add(4);
-//        A.get(1).add(5);
-//        A.get(1).add(6);
-//        A.get(1).add(7);
-//        A.get(1).add(8);
-//        A.get(2).add(9);
-//        A.get(2).add(10);
-//        A.get(2).add(11);
-//        A.get(2).add(12);
-//        System.out.println(2 % 4);
-//        System.out.println(10 % 4);
-//        System.out.println(5 % 4);
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        A.add(0 ,new ArrayList<>());
+        A.add(1,new ArrayList<>());
+        A.add(2 , new ArrayList<>());
+        A.get(0).add(1);
+        A.get(0).add(2);
+        A.get(0).add(3);
+        A.get(0).add(4);
+        A.get(1).add(5);
+        A.get(1).add(6);
+        A.get(1).add(7);
+        A.get(1).add(8);
+        A.get(2).add(9);
+        A.get(2).add(10);
+        A.get(2).add(11);
+        A.get(2).add(12);
+        System.out.println(2 % 4);
+        System.out.println(10 % 4);
+        System.out.println(5 % 4);
 
-//        System.out.println(performOps(A));
-        ArrayList<Integer> A = new ArrayList<>();
-        A.add(5);
-        A.add(10);
-        A.add(2);
-        A.add(1);
-        ArrayList<Integer> B = performOps(A);
+        ArrayList<Integer> A1 = new ArrayList<>();
+        A1.add(5);
+        A1.add(10);
+        A1.add(2);
+        A1.add(1);
+        ArrayList<Integer> B = performOps(A1);
         for (int i = 0; i < B.size(); i++) {
             System.out.print(B.get(i) + " ");
         }
