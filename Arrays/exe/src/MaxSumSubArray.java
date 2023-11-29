a2755227709c27a2ec2bc0e640c8160ccb840f5f
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-11-29 12:13:59 +0530

    MaxSumSubArray

--- Arrays/exe/src/MaxSumSubArray.java
+++ Arrays/exe/src/MaxSumSubArray.java
@@ -2,34 +2,44 @@ import java.util.*;
 public class MaxSumSubArray {
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(0);
-        arr.add(0);
-        arr.add(-1);
-        arr.add(0);
-//        arr.add(2);
-//        arr.add(5);
+        arr.add(1);
+        arr.add(2);
+        arr.add(5);
+        arr.add(-7);
+        arr.add(2);
+        arr.add(5);
         System.out.println(maxset(arr));
     }
 
     public static ArrayList<Integer> maxset(ArrayList<Integer> A) {
         int max = -1;
         int sum = 0;
-        int s = 0;
-        int e = 0;
-        for (int i = 0; i < A.size() - 3; i++) {
-            for(int j = i ; j <  i + 4 ; j++){
-                sum += A.get(j);
-                if ( sum >= max){
-                    max = sum ;
-                    e = j;
-                    s = i;
+        int s = -1;
+        int e = -1;
+        int i = 0;
+        int len = 0;
+        while (i < A.size()){
+                if(A.get(i) >= 0){
+                    sum += A.get(i);
+                    i++;
+                    len++;
                 }
-            }
+                else{
+                    len = 0;
+                    sum = 0;
+                    i++;
+                }
+                if(sum > max ){
+                    max = sum;
+                    e = i;
+                    s = i - len + 1;
+                }
+
         }
         if (max < 0){
             return new ArrayList<>();
         }else{
-            return new ArrayList<>(A.subList(s , e + 1));
+            return new ArrayList<>(A.subList(s , e));
         }
     }
 }
