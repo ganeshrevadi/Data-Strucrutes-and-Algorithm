675ddfa351e0d13ca1504eefbf6309dd4306d22e
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-12-24 11:28:25 +0530

    Implemantation of SubString

--- Strings/src/src/strstr.java
+++ Strings/src/src/strstr.java
@@ -11,15 +11,11 @@ public class strstr {
         int count = 0;
         for (int i = 0; i < A.length(); i++) {
             if(A.charAt(i) == B.charAt(0)){
-                System.out.println("i " + i);
                 for (int j = 0 , k  = i; j < B.length() && k < A.length(); j++ , k++) {
-                    if(A.charAt(k) == B.charAt(j)){
+                    if (A.charAt(k) == B.charAt(j)) {
                         count++;
-                        System.out.println("count " + count);
                     }
                 }
-                System.out.println("Count outside " + count);
-
                 if(count  == B.length()){
                     return i;
                 }
