d1c25678f2bb525b047df651dd8bbebb1cc88846
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-01-19 00:04:49 +0530

    reduce x to zero

---
+++ Arrays/exe/src/ReduceXto0.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ReduceXto0 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static  int minOperations(int[] nums, int x) {
+        int sum = 0;
+        for (int num: nums) sum += num;
+
+        int maxLength = -1, currSum = 0;
+        for (int l=0, r=0; r<nums.length; r++) {
+            currSum += nums[r];
+            while (l <= r && currSum > sum - x) currSum -= nums[l++];
+            if (currSum == sum - x) maxLength = Math.max(maxLength, r-l+1);
+        }
+
+        return maxLength == -1 ? -1 : nums.length - maxLength;
+    }
+}
