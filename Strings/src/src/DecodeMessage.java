5397d6b30446f82461e3d7a16924df47cc533545
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-02-05 23:30:33 +0530

    firstUniqueChar

--- Strings/src/src/DecodeMessage.java
+++ Strings/src/src/DecodeMessage.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class DecodeMessage {
+import java.util.HashMap;
+
+public class DecodeMessage {
+    public static void main(String[] args) {
+        System.out.println(decodeMessage("the quick brown fox jumps over the lazy dog","vkbs bs t suepuv"));
+    }
+
+    public static String decodeMessage(String key, String message) {
+        char ch = 'a';
+        HashMap<Character , Character> map = new HashMap<>();
+        for(char c : key.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c , map.get(c));
+            }
+            else{
+                map.put(c , ch);
+                ch = (char)((int)ch + 1);
+            }
+        }
+        System.out.println(map);
+        return "";
+    }
 }
---
+++ Strings/src/src/firstUniqueChar.java
@@ -0,0 +1,22 @@
+import java.util.LinkedHashMap;
+
+public class firstUniqueChar {
+    public static void main(String[] args) {
+
+    }
+
+    public static int firstUniqChar(String s) {
+        LinkedHashMap<Character , Integer> map = new LinkedHashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
