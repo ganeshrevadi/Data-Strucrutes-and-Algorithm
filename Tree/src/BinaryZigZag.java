855e49829784692db6e70c14e8061ca06d4601c6
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-05-07 21:40:47 +0530

    ZigZag Tree

--- Tree/src/BinaryZigZag.java
+++ Tree/src/BinaryZigZag.java
@@ -6,13 +6,30 @@ public class BinaryZigZag {
 
     public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
             List<List<Integer>> ls = new ArrayList<>();
-            return travers(root, ls);
+            ls.add(new ArrayList<>(root.val));
+            return travers(root, ls, -1);
     }
 
-    public static List<List<Integer>> travers(TreeNode root, List<List<Integer>> ls){
+    public static List<List<Integer>> travers(TreeNode root, List<List<Integer>> ls,int flag){
         if(root == null) return ls;
 
+        if(flag == -1){
+            ArrayList<Integer> ar = new ArrayList<>();
+            ar.add(root.left.val);
+            ar.add(root.right.val);
+            ls.add(ar);
 
+        }
+        else {
+            ArrayList<Integer> ar = new ArrayList<>();
+            ar.add(root.right.val);
+            ar.add(root.left.val);
+            ls.add(ar);
+        }
+        travers(root.left, ls, flag * -1);
+        travers(root.right, ls, flag * -1);
+
+        return ls;
     }
 
 }
