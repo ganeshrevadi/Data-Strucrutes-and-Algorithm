bb25f825a6eac893a9bef9633a2c1c42b610357a
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-05-09 14:41:14 +0530

    Left_Right_view

---
+++ Tree/src/Left_Right_View.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Left_Right_View {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> rightSideView(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        recursionRight(root, 0, res);
+        return res;
+    }
+
+    public static void recursionRight(TreeNode root, int level, List<Integer> res){
+        if(root == null){
+            return;
+        }
+
+        if(res.size() == level){
+            res.add(root.val);
+        }
+
+
+        recursionRight(root.right, level + 1, res);
+        recursionRight(root.left, level + 1, res);
+    }
+
+
+
+
+
+
+
+}
