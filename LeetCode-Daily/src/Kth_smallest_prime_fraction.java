798ef405905323e6ceb575bd71efb6bba5cabf88
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-05-10 18:31:23 +0530

    786. Kth Smallest prime fraction

--- LeetCode-Daily/src/Kth_smallest_prime_fraction.java
+++ LeetCode-Daily/src/Kth_smallest_prime_fraction.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Pair<A, B> {
+    public A first;
+    public B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+}
 
 public class Kth_smallest_prime_fraction {
     public static void main(String[] args) {
@@ -9,18 +20,19 @@ public class Kth_smallest_prime_fraction {
 
 
     public static  int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        ArrayList<Float> a = new ArrayList<>();
+        PriorityQueue<Pair<Double, Pair<Integer, Integer>>> pq = new PriorityQueue<>((pair1, pair2) ->
+                Double.compare(pair2.first, pair1.first));
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if(i < j){
-                    a.add((float)(arr[i] / arr[j]));
-                }
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                double val = (arr[i] * 1.0) / arr[j];
+                pq.add(new Pair<>(val, new Pair<>(i, j)));
+                if(pq.size() > k) pq.poll();
             }
         }
 
-        Collections.sort(a);
+        Pair<Integer, Integer> se = pq.poll().second;
 
-        return new int[1];
+        return new int[]{arr[se.first], arr[se.second]};
     }
 }
