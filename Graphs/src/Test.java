89d90ca536cdeac54fce92525e3c18fef03443ab
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-08-04 11:05:57 +0530

    Test

--- Graphs/src/Test.java
+++ Graphs/src/Test.java
@@ -1,7 +1,24 @@
 import java.security.spec.ECField;
 
-public class Test {
+
+// Inheriting the interface we use the implements keyword
+interface Fest{
+    void caller();
+}
+
+// To inherit the abstract class we use the extends keyword
+abstract class Inheri{
+    public void tester(){
+        System.out.println("Tester");
+    }
+
+    abstract public void solver();
+}
+
+
+public class Test implements Fest{
     public static void main(String[] args) {
+        Test n = new Test();
         try {
             int[] ar = new int[3];
             System.out.println(ar[4]);
@@ -14,5 +31,20 @@ public class Test {
         }
 
         System.out.println("I'm outside finally");
+
+    }
+
+    public void solver(){
+
+    }
+    public  void caller() {
+        System.out.println("I'm the caller");
     }
 }
+
+
+
+
+
+
+
